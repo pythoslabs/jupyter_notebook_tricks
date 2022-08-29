ee754c4832359105715aa2c1c8c67b25cff3926f
Pythos Labs <48703756+pythoslabs@users.noreply.github.com>
2022-08-29 21:49:41 +0530

    Create tips.py

---
+++ tips.py
@@ -0,0 +1,6 @@
+#1. To enable running & printing output of all the commands in the cell  rather than just the last command
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+#2. import another jupyter notebook 
+# eg: a uttlities helper file with lots of functions 
